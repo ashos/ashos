0e325358bc7cd6f2f611a64b1f07c0c04798fd77
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-08-16 10:46:01 +0200

    typo fix

--- astpk.py
+++ astpk.py
@@ -535,7 +535,7 @@ def delete(snapshot):
         os.system(f"btrfs sub del /.snapshots/etc/etc-{snapshot} >/dev/null 2>&1")
 #        os.system(f"btrfs sub del /.snapshots/var/var-{snapshot} >/dev/null 2>&1")
         os.system(f"btrfs sub del /.snapshots/rootfs/snapshot-{snapshot} >/dev/null 2>&1")
-        if (os.path.exists(f"/.snapshots/rootfs/snapshot-chr{snaphot}")):
+        if (os.path.exists(f"/.snapshots/rootfs/snapshot-chr{snapshot}")):
             os.system(f"btrfs sub del /.snapshots/boot/boot-chr{snapshot} >/dev/null 2>&1")
             os.system(f"btrfs sub del /.snapshots/etc/etc-chr{snapshot} >/dev/null 2>&1")
             os.system(f"btrfs sub del /.snapshots/rootfs/snapshot-chr{snapshot} >/dev/null 2>&1")
@@ -544,7 +544,7 @@ def delete(snapshot):
             os.system(f"btrfs sub del /.snapshots/etc/etc-{child} >/dev/null 2>&1")
  #           os.system(f"btrfs sub del /.snapshots/var/var-{child} >/dev/null 2>&1")
             os.system(f"btrfs sub del /.snapshots/rootfs/snapshot-{child} >/dev/null 2>&1")
-            if (os.path.exists(f"/.snapshots/rootfs/snapshot-chr{snaphot}")):
+            if (os.path.exists(f"/.snapshots/rootfs/snapshot-chr{snapshot}")):
                 os.system(f"btrfs sub del /.snapshots/boot/boot-chr{snapshot} >/dev/null 2>&1")
                 os.system(f"btrfs sub del /.snapshots/etc/etc-chr{snapshot} >/dev/null 2>&1")
                 os.system(f"btrfs sub del /.snapshots/rootfs/snapshot-chr{snapshot} >/dev/null 2>&1")
