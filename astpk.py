0b6e37d11ff69a5b0e61e6328e5e803346b2f0e2
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-08-19 21:20:52 +0200

    more fixing and cleanup

--- astpk.py
+++ astpk.py
@@ -1060,11 +1060,11 @@ def main(args):
         csnapshot = args_2[0]
         args_2.remove(args_2[0])
         excode = install(csnapshot, str(" ").join(args_2))
-        options_live = get_persnap_options(tmp)
-        aur = False
-        if options_live["aur"] == "True":
-            aur = True
         if live and not excode: # only perform the live_install if the first install was successful
+            options_live = get_persnap_options(get_tmp())
+            aur = False
+            if options_live["aur"] == "True":
+                aur = True
             live_install(str(" ").join(args_2), aur)
     elif arg == "run":
         args_2 = args
