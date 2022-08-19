891844eaf0ae1abb7b5bb134f6e8fc9329598ccb
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-08-19 11:39:49 +0200

    replace arch-chroot

--- astpk.py
+++ astpk.py
@@ -977,23 +977,24 @@ def aur_setup_live(snap):
     os.system(f"mount --bind /var /.snapshots/rootfs/snapshot-{tmp}/var >/dev/null 2>&1")
     os.system(f"mount --bind /etc /.snapshots/rootfs/snapshot-{tmp}/etc >/dev/null 2>&1")
     os.system(f"mount --bind /tmp /.snapshots/rootfs/snapshot-{tmp}/tmp >/dev/null 2>&1")
-    excode = int(os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} pacman -S --noconfirm --needed sudo git base-devel"))
+
+    excode = int(os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} pacman -S --noconfirm --needed sudo git base-devel"))
     if excode:
         return excode
-    os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} useradd aur")
+    os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} useradd aur")
     os.system(f"chmod +w /.snapshots/rootfs/snapshot-{snap}/etc/sudoers")
     os.system(f"echo 'aur ALL=(ALL:ALL) NOPASSWD: ALL' >> /.snapshots/rootfs/snapshot-{snap}/etc/sudoers")
     os.system(f"chmod -w /.snapshots/rootfs/snapshot-{snap}/etc/sudoers")
-    os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} mkdir -p /home/aur")
-    os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} chown -R aur /home/aur >/dev/null 2>&1")
+    os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} mkdir -p /home/aur")
+    os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} chown -R aur /home/aur >/dev/null 2>&1")
     # TODO: no error checking here
-    excode = int(os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} su aur -c 'rm -rf /home/aur/paru-bin && cd /home/aur && git clone https://aur.archlinux.org/paru-bin.git' >/dev/null 2>&1"))
+    excode = int(os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} su aur -c 'rm -rf /home/aur/paru-bin && cd /home/aur && git clone https://aur.archlinux.org/paru-bin.git' >/dev/null 2>&1"))
     if excode:
         os.system(f"umount /.snapshots/rootfs/snapshot-{tmp}/* >/dev/null 2>&1")
         os.system(f"umount /.snapshots/rootfs/snapshot-{tmp} >/dev/null 2>&1")
         print("F: failed to download paru-bin")
         return excode
-    excode = int(os.system(f"arch-chroot /.snapshots/rootfs/snapshot-{snap} su aur -c 'cd /home/aur/paru-bin && makepkg -si'"))
+    excode = int(os.system(f"chroot /.snapshots/rootfs/snapshot-{snap} su aur -c 'cd /home/aur/paru-bin && makepkg -si'"))
     if excode:
         os.system(f"umount /.snapshots/rootfs/snapshot-{tmp}/* >/dev/null 2>&1")
         os.system(f"umount /.snapshots/rootfs/snapshot-{tmp} >/dev/null 2>&1")
