b86f41d690632177c92c36753b6444abff3f2737
Jan Novotný <cuberjan3@email.cz>
2022-10-03 19:40:01 +0200

    Added licensing to files

--- src/include/cmd.h
+++ src/include/cmd.h
@@ -1,6 +1,25 @@
+/*
+AshOS Command interfacing library
+Copyright (C) 2022  AshOS
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	original author: Jan Novotný (https://github.com/CuBeRJAN)
+*/
 #ifndef __CMD_H_
 #define __CMD_H_
-#define _GNU_SOURCE // for asprintf
+//#define _GNU_SOURCE // for asprintf
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
