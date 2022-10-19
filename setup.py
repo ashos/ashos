41742a0e06852a70c118589cb0a4595136704c45
Reza <3228126+i2@users.noreply.github.com>
2022-10-19 14:00:37 -0300

    Fix IndexError for BIOS setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
 
+import os.path
 import subprocess
 import sys
 
-args = list(sys.argv[0:4]) # just first 3 arguments (exclude distro arguments)
+is_efi = os.path.exists("/sys/firmware/efi")
 use_other_iso = "" # if using different iso to install target OS, use its id
 
-try: # If distro to install not same as iso, pass arguments 4 and 5 (see README)
-    distro = sys.argv[4]
-    distro_name = sys.argv[5]
+try:
+    if is_efi:
+        args = list(sys.argv[0:4]) # just first 3 arguments (exclude distro arguments)
+        distro = sys.argv[4]
+        distro_name = sys.argv[5]
+    else:
+        args = list(sys.argv[0:3]) # just first 2 arguments (exclude distro arguments)
+        distro = sys.argv[3]
+        distro_name = sys.argv[4]
 except IndexError:
     distro = subprocess.check_output(['./src/detect_os.sh', 'id']).decode('utf-8').replace('"', "").strip()
     distro_name = subprocess.check_output(['./src/detect_os.sh', 'name']).decode('utf-8').replace('"', "").strip()
 
 if distro:
-    try: # CAUTION: comment lines 17-24 if prepared manually
+    try: # CAUTION: comment lines 24-35 if prepared manually
         if use_other_iso != "":
-            subprocess.check_output([f'./src/prep/{use_other_iso}_live.sh', f'{args[1]}', f'{args[2]}', f'{args[3]}'])
+            use_distro = use_other_iso
         else:
-            subprocess.check_output([f'./src/prep/{distro}_live.sh', f'{args[1]}', f'{args[2]}', f'{args[3]}'])
+            use_distro = distro
+        if is_efi:
+            subprocess.check_output([f'./src/prep/{use_distro}_live.sh', f'{args[1]}', f'{args[2]}', f'{args[3]}'])
+        else:
+            subprocess.check_output([f'./src/prep/{use_distro}_live.sh', f'{args[1]}', f'{args[2]}'])
     except subprocess.CalledProcessError as e:
         print(f"F: There was an error in prep steps! {e.output.decode('utf-8')}")
     else:
