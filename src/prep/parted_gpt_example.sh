82ebf86e83796bdeca085da6bed76dfc0955bab9
Reza <3228126+i2@users.noreply.github.com>
2022-11-02 00:22:42 -0300

    2022/11/02 - 00:22:42

--- src/prep/parted_gpt_example.sh
+++ src/prep/parted_gpt_example.sh
@@ -10,6 +10,11 @@ fi
 parted --align minimal --script $1 mklabel gpt unit MiB \
         mkpart ESP fat32 0% 256 set 1 boot on \
         mkpart primary ext4 256 80% \
-        mkpart primary ext4 80% 90%
+        mkpart primary ext4 80% 100%
 mkfs.vfat -F32 -n EFI ${1}1
+
+# External boot partition
+#parted --align minimal --script $2 mklabel gpt unit MiB \
+        mkpart primary ext4 0% 100%
+#mkfs.btrfs -L BOOT ${2}1
 
