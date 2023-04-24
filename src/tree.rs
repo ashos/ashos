13ebda813632f94bec68d5465f3f0f0b2e78412a
ochibani <11yzyv86j@relay.firefox.com>
2023-04-24 19:06:52 +0200

    add recurse_tree & remove_node

--- src/tree.rs
+++ src/tree.rs
@@ -1,6 +1,7 @@
 use cpython::{NoArgs, ObjectProtocol, PyDict, Python};
 use std::fs::{File, OpenOptions, read_to_string};
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Write, Read};
+use std::path::Path;
 
 
 // Clone within node
@@ -141,94 +142,93 @@ fn import_tree_file(treename: &str) -> Result<cpython::PyObject, cpython::PyErr>
     tree_file
 }
 
-// Print out tree with descriptions
-pub fn print_tree() {
+// Print out tree with descriptions //REVIEW
+/*pub fn print_tree() {
     let tree =fstree().unwrap();
     let snapshot = get_current_snapshot();
     let gil = Python::acquire_gil();
     let py = gil.python();
+    let id_dict = PyDict::new(py);
+    id_dict.set_item(py, "id", &snapshot).unwrap();
 
     // From anytree import AsciiStyle, RenderTree
     let anytree =  py.import("anytree").unwrap();
     let asciistyle = anytree.call(py, "AsciiStyle", NoArgs, None).unwrap();
     let style = PyDict::new(py);
     style.set_item(py, "style", asciistyle).unwrap();
-    let rendertree = anytree.call(py, "RenderTree", (tree,), Some(&style)).unwrap();
-    //for pre, fill, node in RenderTree(tree, style=AsciiStyle()):
-        //if os.path.isfile(f"/.snapshots/ash/snapshots/{node.name}-desc"):
-            //with open(f"/.snapshots/ash/snapshots/{node.name}-desc", "r") as descfile:
-                //desc = descfile.readline()
-        //else:
-            //desc = ""
-        //if str(node.name) == "0":
-            //desc = "base snapshot"
-        //if snapshot != str(node.name):
-            //print("%s%s - %s" % (pre, node.name, desc))
-        //else:
-    //print("%s%s*- %s" % (pre, node.name, desc))
+    let rendertree = anytree.call(py, "RenderTree", (&tree,), Some(&style)).unwrap().iter(py).unwrap();
+
+    // Filter as kwarg
+    let filter = py.eval("lambda node: ('x'+str(node.name)+'x') in ('x'+str(id)+'x')", Some(&id_dict), None).unwrap();
+    let filter_ = PyDict::new(py);
+    filter_.set_item(py, "filter_", filter).unwrap();
+
+    // Parent value
+    let par = anytree.call(py, "find", (tree,), Some(&filter_)).unwrap();
+
+    // Parent as kwarg
+    let parent = PyDict::new(py) ;
+    parent.set_item(py, "parent", par).unwrap();
+
+    // Node value
+    let val: i32 = snapshot.parse().unwrap();
+    let node = anytree.call(py, "Node", (val,), Some(&parent)).unwrap();
+    let node_name = node.getattr(py, "name").unwrap().to_string();
+
+    for (pre, fill, node) in rendertree {
+        if Path::new(&format!("/.snapshots/ash/snapshots/{}-desc", node_name)).is_file() {
+            let descfile = File::open(format!("/.snapshots/ash/snapshots/{}-desc", node_name)).unwrap();
+            let mut desc = String::new();
+            descfile.read_to_string(&mut desc).unwrap();
+        } else {
+            let desc = "";
+        }
+        if node_name == "0" {
+            let desc = "base snapshot";
+            if snapshot != node_name {
+                println!("{}{}-{}", pre,node_name,desc);
+            } else {
+                println!("{}{}*-{}", pre,node_name,desc);//REVIEW
+            }
+        }
+    }
+}*/
+
+// Return order to recurse tree
+pub fn recurse_tree(cid: &str) -> Vec<String> {
+    let mut order: Vec<String> = Vec::new();
+    for child in return_children(cid) {
+        let par = get_parent(&child).unwrap().to_string();
+        if child != cid {
+            order.push(par);
+            order.push(child);
+        }
+    }
+    return order;
 }
 
-//#   Return order to recurse tree
-//def recurse_tree(tree, cid):
-    //order = []
-    //for child in (return_children(tree, cid)):
-        //par = get_parent(tree, child)
-        //if child != cid:
-            //order.append(par)
-            //order.append(child)
-    //return (order)
+// Remove node from tree
+pub fn remove_node(id: &str) -> Result<cpython::PyObject, cpython::PyErr> {
+    let tree = fstree().unwrap();
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let id_dict = PyDict::new(py);
+    id_dict.set_item(py, "id", id).unwrap();
 
-//#   Remove node from tree
-//def remove_node(tree, id):
-    //par = (find(tree, filter_=lambda node: ("x"+str(node.name)+"x") in ("x"+str(id)+"x")))
-    //par.parent = None
+    // Import anytree
+    let anytree =  py.import("anytree").unwrap();
 
-//#   Return all children for node
-//def return_children(tree, id):
-    //children = []
-    //par = (find(tree, filter_=lambda node: ("x"+str(node.name)+"x") in ("x"+str(id)+"x")))
-    //for child in PreOrderIter(par):
-        //children.append(child.name)
-    //if id in children:
-        //children.remove(id)
-    //return (children)
+    // Filter as kwarg
+    let filter = py.eval("lambda node: ('x'+str(node.name)+'x') in ('x'+str(id)+'x')", Some(&id_dict), None).unwrap();
+    let filter_ = PyDict::new(py);
+    filter_.set_item(py, "filter_", filter).unwrap();
 
-//#   Sync tree helper function ### REVIEW might need to put it in distro-specific ashpk.py
-//def sync_tree_helper(CHR, s_f, s_t):
-    //os.system("mkdir -p /.snapshots/tmp-db/local/") ### REVIEW Still resembling Arch pacman folder structure!
-    //os.system("rm -rf /.snapshots/tmp-db/local/*") ### REVIEW
-    //pkg_list_to = pkg_list(CHR, s_t)
-    //pkg_list_from = pkg_list("", s_f)
-  //# Get packages to be inherited
-    //pkg_list_from = [j for j in pkg_list_from if j not in pkg_list_to]
-    //os.system(f"cp -r /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/. /.snapshots/tmp-db/local/") ### REVIEW
-    //os.system(f"cp -n -r --reflink=auto /.snapshots/rootfs/snapshot-{s_f}/. /.snapshots/rootfs/snapshot-{CHR}{s_t}/{DEBUG}")
-    //os.system(f"rm -rf /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/*") ### REVIEW
-    //os.system(f"cp -r /.snapshots/tmp-db/local/. /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/") ### REVIEW
-    //for entry in pkg_list_from:
-        //os.system(f"bash -c 'cp -r /.snapshots/rootfs/snapshot-{s_f}/usr/share/ash/db/local/{entry}-[0-9]* /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/'") ### REVIEW
-    //os.system("rm -rf /.snapshots/tmp-db/local/*") ### REVIEW (originally inside the loop, but I took it out)
-
-//#   Recursively run an update in tree
-//def update_tree(tree, treename):
-    //if not os.path.exists(f"/.snapshots/rootfs/snapshot-{treename}"):
-        //print(f"F: Cannot update as tree {treename} doesn't exist.")
-    //else:
-        //upgrade(treename)
-        //order = recurse_tree(tree, treename)
-        //if len(order) > 2:
-            //order.remove(order[0])
-            //order.remove(order[0])
-        //while True:
-            //if len(order) < 2:
-                //break
-            //arg = order[0]
-            //sarg = order[1]
-            //print(arg, sarg)
-            //order.remove(order[0])
-            //order.remove(order[0])
-            //auto_upgrade(sarg)
-        //print(f"Tree {treename} updated.")
+    // Parent value
+    let parent: Option<String> = None;
+    let par = anytree.call(py, "find", (tree,), Some(&filter_)).unwrap();
+    par.setattr(py, "parent", parent).unwrap();
+    par.getattr(py, "parent")
+}
 
 // Return all children for node
 pub fn return_children(id: &str) -> Vec<String> {
@@ -261,6 +261,22 @@ pub fn return_children(id: &str) -> Vec<String> {
     }
     children
 }
+
+//#   Sync tree helper function ### REVIEW might need to put it in distro-specific ashpk.py
+//def sync_tree_helper(CHR, s_f, s_t):
+    //os.system("mkdir -p /.snapshots/tmp-db/local/") ### REVIEW Still resembling Arch pacman folder structure!
+    //os.system("rm -rf /.snapshots/tmp-db/local/*") ### REVIEW
+    //pkg_list_to = pkg_list(CHR, s_t)
+    //pkg_list_from = pkg_list("", s_f)
+  //# Get packages to be inherited
+    //pkg_list_from = [j for j in pkg_list_from if j not in pkg_list_to]
+    //os.system(f"cp -r /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/. /.snapshots/tmp-db/local/") ### REVIEW
+    //os.system(f"cp -n -r --reflink=auto /.snapshots/rootfs/snapshot-{s_f}/. /.snapshots/rootfs/snapshot-{CHR}{s_t}/{DEBUG}")
+    //os.system(f"rm -rf /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/*") ### REVIEW
+    //os.system(f"cp -r /.snapshots/tmp-db/local/. /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/") ### REVIEW
+    //for entry in pkg_list_from:
+        //os.system(f"bash -c 'cp -r /.snapshots/rootfs/snapshot-{s_f}/usr/share/ash/db/local/{entry}-[0-9]* /.snapshots/rootfs/snapshot-{CHR}{s_t}/usr/share/ash/db/local/'") ### REVIEW
+    //os.system("rm -rf /.snapshots/tmp-db/local/*") ### REVIEW (originally inside the loop, but I took it out)
 
 // Save tree to file
 pub fn write_tree() -> Result<(), std::io::Error> {
