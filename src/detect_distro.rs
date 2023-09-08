a9a07c74b411ee7bcf6b3245dfd7bfccbc3d45fe
ochibani <11yzyv86j@relay.firefox.com>
2023-09-08 19:34:21 +0200

    update detect_distro

--- src/detect_distro.rs
+++ src/detect_distro.rs
@@ -1,13 +1,26 @@
 use std::fs::{read_dir, read_to_string};
 
-// Detect distro
+// Capitalize first letter
+fn capitalize(s: &str) -> String {
+    let mut chars = s.chars();
+    if let Some(first_char) = chars.next() {
+        let capitalized = first_char.to_uppercase().chain(chars).collect();
+        return capitalized;
+    }
+    String::new()
+}
+
+// Detect distro ID
 pub fn distro_id() -> String {
     let mut distro_id = String::new();
     // Check if /etc/lsb-release exists and contains DISTRIB_ID
     if let Ok(file) = read_to_string("/etc/lsb-release") {
         for line in file.lines() {
             if line.starts_with("DISTRIB_ID=") {
-                distro_id = line.split('=').nth(1).unwrap().to_lowercase().to_string();
+                distro_id = line.split('=').nth(1).unwrap()
+                                                  .to_lowercase()
+                                                  .trim_matches('"')
+                                                  .to_string();
                 break;
             }
         }
@@ -18,7 +31,10 @@ pub fn distro_id() -> String {
         if let Ok(file) = read_to_string("/etc/os-release") {
             for line in file.lines() {
                 if line.starts_with("ID=") {
-                    distro_id = line.split('=').nth(1).unwrap().to_lowercase().to_string();
+                    distro_id = line.split('=').nth(1).unwrap()
+                                                      .to_lowercase()
+                                                      .trim_matches('"')
+                                                      .to_string();
                     break;
                 }
             }
@@ -31,7 +47,13 @@ pub fn distro_id() -> String {
             if let Ok(entry) = entry {
                 let path = entry.path();
                 if path.is_file() && path.to_str().unwrap().ends_with("-release") {
-                    distro_id = path.file_stem().unwrap().to_str().unwrap().to_lowercase().to_owned().to_string();
+                    distro_id = path.file_stem().unwrap()
+                                                .to_str().unwrap()
+                                                         .to_lowercase().to_owned().to_string()
+                                                                                   .split('-')
+                                                                                   .nth(0)
+                                                                                   .unwrap()
+                                                                                   .to_string();
                     break;
                 }
             }
@@ -40,13 +62,18 @@ pub fn distro_id() -> String {
     distro_id
 }
 
+// Detect distro name
 pub fn distro_name() -> String {
     let mut distro_name = String::new();
     // Check if /etc/lsb-release exists and contains DISTRIB_NAME
     if let Ok(file) = read_to_string("/etc/lsb-release") {
         for line in file.lines() {
             if line.starts_with("DISTRIB_DESCRIPTION=") {
-                distro_name = line.split('=').nth(1).unwrap().to_string();
+                let distro_name_from_path = line.split('=').nth(1).unwrap()
+                                                                  .to_lowercase().split(' ').nth(0).unwrap()
+                                                                                                   .trim_matches('"')
+                                                                                                   .to_string();
+                distro_name = capitalize(&distro_name_from_path);
                 break;
             }
         }
@@ -57,7 +84,31 @@ pub fn distro_name() -> String {
         if let Ok(file) = read_to_string("/etc/os-release") {
             for line in file.lines() {
                 if line.starts_with("NAME=") {
-                    distro_name = line.split('=').nth(1).unwrap().to_string();
+                    let distro_name_from_path = line.split('=').nth(1).unwrap()
+                                                                      .to_lowercase().split(' ').nth(0).unwrap()
+                                                                                                       .trim_matches('"')
+                                                                                                       .to_string();
+                    distro_name = capitalize(&distro_name_from_path);
+                    break;
+                }
+            }
+        }
+    }
+
+    // If both checks fail, loop through all files in /etc/ and check for -release files
+    if distro_name.is_empty() {
+        for entry in read_dir("/etc").unwrap() {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() && path.to_str().unwrap().ends_with("-release") {
+                    let distro_name_from_path = path.file_stem().unwrap()
+                                                                .to_str().unwrap()
+                                                                         .to_lowercase().to_owned().to_string()
+                                                                                                   .split('-')
+                                                                                                   .nth(0)
+                                                                                                   .unwrap()
+                                                                                                   .to_string();
+                    distro_name = capitalize(&distro_name_from_path);
                     break;
                 }
             }
