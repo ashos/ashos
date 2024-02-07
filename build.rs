2ad923cc7da4f672e932371a1bed7181e055cb3c
ochibani <11yzyv86j@relay.firefox.com>
2024-02-08 01:43:58 +0200

    Add auto-complete

---
+++ build.rs
@@ -0,0 +1,19 @@
+use clap::ValueEnum;
+use clap_complete::{generate_to, Shell};
+use std::io::Error;
+
+include!("src/cli.rs");
+
+fn main() -> Result<(), Error> {
+    let out_dir = match std::env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(out_dir) => out_dir,
+    };
+
+    let mut cmd = cli();
+    for &shell in Shell::value_variants() {
+        generate_to(shell, &mut cmd, "ash", &out_dir)?;
+    }
+
+    Ok(())
+}
